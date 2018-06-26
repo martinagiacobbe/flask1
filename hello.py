0a47e30831ac105cd84be57b488ec8df64faf6e4
Martina <martinagiacobbe@gmail.com>
2018-06-26 10:07:37 +0200

    start

--- hello.py
+++ hello.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request, json
+from flaskext.mysql  import MySQL
 app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
+
+def main():
+    return render_template('index2.html')
+
+
 def hello():
     return "Hello world"
 if __name__=="__main__":
